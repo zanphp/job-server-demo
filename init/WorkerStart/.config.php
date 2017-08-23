8c0c267e4450a16c1ce95f3127c52e2903cd0b33
chuxiaofeng <chuxiaofeng@youzan.com>
2017-08-23 22:09:09 +0800

    change ns

--- init/WorkerStart/.config.php
+++ init/WorkerStart/.config.php
@@ -1,10 +1,10 @@
 <?php
 
 use Zan\Framework\Components\JobServer\WorkerStart\InitializeJobServer;
-use Zan\Framework\Components\Nsq\InitializeSQS;
+use ZanPHP\NSQ\InitializeNSQ;
 
 return [
     // !!! 注意配置顺序, SQS要优先JobServer初始化
-    InitializeSQS::class,
+    InitializeNSQ::class,
     InitializeJobServer::class,
 ];
